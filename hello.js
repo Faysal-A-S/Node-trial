545d62af9275c46f99ca048ff6738d43fc9d620a
Faysal Ahmed <hl.faysal1@gmail.com>
2020-01-07 23:58:22 +0600

    initial  Commit

---
+++ hello.js
@@ -0,0 +1,26 @@
+let http=require('http')
+
+let  url=require('url')
+
+let fs=require('fs')
+
+http.createServer((req,res)=>{
+
+    if(req.url==='/favicon.ico'||req.url==='/'){
+        req.url='/index'
+    }
+
+    let q =url.parse(req.url,true);
+    console.table(q);
+    let filename='.'+q.pathname;
+    filename=filename+'.html'
+    fs.readFile(filename,(err,data)=>{
+        if(err) {
+            res.writeHead(404,{'Content-Type':'text/html'});
+            return res.end('error 404 not found')
+        }
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    })
+}).listen(5000);
